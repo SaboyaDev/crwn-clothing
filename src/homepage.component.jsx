c8241050882cc5d902fe6628e0a5fb641f8e2400
SaboyaDev <saboyadev@gmail.com>
2022-02-02 00:39:19 -0500

    cleaned up the files created with CRA and created all the files for the homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const HomePage = () => {
+	return (
+		<div className='homepage'>
+			<div className='directory-menu'>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>hats</h1>
+						<span className='subtitle'>shop Now</span>
+					</div>
+				</div>
+
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>jacket</h1>
+						<span className='subtitle'>shop Now</span>
+					</div>
+				</div>
+
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>womens</h1>
+						<span className='subtitle'>shop Now</span>
+					</div>
+				</div>
+
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>mens</h1>
+						<span className='subtitle'>shop Now</span>
+					</div>
+				</div>
+
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>Hats</h1>
+						<span className='subtitle'>Shop Now</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default HomePage

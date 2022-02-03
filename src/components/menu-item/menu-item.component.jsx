bcb1b16ffd0a1e75b87947eb3a1585bb622c6a68
SaboyaDev <saboyadev@gmail.com>
2022-02-03 15:13:58 -0500

    added the hover efect on the menu items, background image grows and and center content box's background color gets more solid

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,17 @@
 import './menu-item.styles.scss'
 
 export const MenuItem = ({ title, imageUrl, size }) => {
-	console.log(imageUrl)
 	return (
-		<div
-			style={{
-				backgroundImage: `url(${imageUrl})`,
-			}}
-			className={`${size} menu-item`}
-		>
+		<div className={`${size} menu-item`}>
+			<div
+				style={{
+					backgroundImage: `url(${imageUrl})`,
+				}}
+				className='background-image'
+			/>
 			<div className='content'>
-				<h1 className='title'>{title}</h1>
-				<span className='subtitle'>shop now</span>
+				<h1 className='title'>{title.toUpperCase()}</h1>
+				<span className='subtitle'>{`shop now`.toUpperCase()}</span>
 			</div>
 		</div>
 	)
